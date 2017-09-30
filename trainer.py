941a15a69021764295f2fd0a50093dc12ae063ca
sean <seann999@gmail.com>
2017-09-30 22:55:09 +0900

    initial

---
+++ trainer.py
@@ -0,0 +1,65 @@
+import torch 
+import torch.nn as nn
+import torchvision.datasets as dsets
+import torchvision.transforms as transforms
+from torch.autograd import Variable
+
+from model import CNN
+
+# Hyper Parameters
+num_epochs = 5
+batch_size = 100
+learning_rate = 0.001
+
+n_classes = 2
+train_dataset = dsets.ImageFolder(root='~/../d-hacks/data/compartment_line/torch_dataset/train', transform=transforms.ToTensor())
+test_dataset = dsets.ImageFolder(root='~/../d-hacks/data/compartment_line/torch_dataset/train', transform=transforms.ToTensor())
+
+# Data Loader (Input Pipeline)
+train_loader = torch.utils.data.DataLoader(dataset=train_dataset,
+                                           batch_size=batch_size, 
+                                           shuffle=True)
+
+test_loader = torch.utils.data.DataLoader(dataset=test_dataset,
+                                          batch_size=batch_size, 
+                                          shuffle=False)
+        
+cnn = CNN()
+cnn.cuda()
+
+# Loss and Optimizer
+criterion = nn.CrossEntropyLoss()
+optimizer = torch.optim.Adam(cnn.parameters(), lr=learning_rate)
+
+# Train the Model
+for epoch in range(num_epochs):
+    for i, (images, labels) in enumerate(train_loader):
+        images = Variable(images).cuda()
+        labels = Variable(labels).cuda()
+        
+        # Forward + Backward + Optimize
+        optimizer.zero_grad()
+        outputs = cnn(images)
+        loss = criterion(outputs, labels)
+        loss.backward()
+        optimizer.step()
+        
+        if (i+1) % 100 == 0:
+            print ('Epoch [%d/%d], Iter [%d/%d] Loss: %.4f' 
+                   %(epoch+1, num_epochs, i+1, len(train_dataset)//batch_size, loss.data[0]))
+
+# Test the Model
+cnn.eval()    # Change model to 'eval' mode (BN uses moving mean/var).
+correct = 0
+total = 0
+for images, labels in test_loader:
+    images = Variable(images).cuda()
+    outputs = cnn(images)
+    _, predicted = torch.max(outputs.data, 1)
+    total += labels.size(0)
+    correct += (predicted.cpu() == labels).sum()
+
+print('Test Accuracy of the model on the 10000 test images: %d %%' % (100 * correct / total))
+
+# Save the Trained Model
+torch.save(cnn.state_dict(), 'cnn.pkl')
