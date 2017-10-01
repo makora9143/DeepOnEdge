c0f535873903b2a864ba6c9c1ab43f1f3d775723
makora9143 <makoto.kawano@gmail.com>
2017-10-01 16:22:31 +0900

    classesの対応順を変更

--- demo.py
+++ demo.py
@@ -15,7 +15,7 @@ def start_cam(cap):
     ckpt = torch.load('cnn.pkl')
 
     cnn.load_state_dict(ckpt['model'])
-    classes = ["no damage", "no line", "damage"]
+    classes = ["no damage", "damage", "no line"]
     colors = [(0, 0, 255), (255, 255, 255), (0, 255, 0)]
 
     cnn.cuda()
